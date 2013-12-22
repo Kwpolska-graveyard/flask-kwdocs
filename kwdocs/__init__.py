e5bff32fddb95bdceb7c170f50ed00200cfed188
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-12-22 20:21:41 +0100

    add standard headers
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- kwdocs/__init__.py
+++ kwdocs/__init__.py
@@ -1,6 +1,53 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""KwDocs -- A LaTeX document management system for Flask."""
+# -*- encoding: utf-8 -*-
+# Flask-KwDocs v0.1.0
+# A LaTeX document management system for Flask.
+# Copyright © 2013, Chris “Kwpolska” Warrick.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions, and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions, and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the author of this software nor the names of
+#    contributors to this software may be used to endorse or promote
+#    products derived from this software without specific prior written
+#    consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+    flask-kwdocs
+    ~~~~~~~~~~~~
+
+    A LaTeX document management system for Flask.
+
+    :Copyright: © 2013, Chris “Kwpolska” Warrick.
+    :License: BSD (see /LICENSE).
+"""
+
+__title__ = 'Flask-KwDocs'
+__version__ = '0.1.0'
+__author__ = 'Chris “Kwpolska” Warrick'
+__license__ = '3-clause BSD'
+__docformat__ = 'restructuredtext en'
 
 from kwlh import app, db
 from flask import (Blueprint, request, flash, render_template,
